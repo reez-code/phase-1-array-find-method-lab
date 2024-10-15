f69cf1776210d0b62ce72214aa11ba799183cfb9
Phil <phil.github@gmail.com>
2024-10-15 12:17:27 +0300

    practise exercise done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(array) {
+  const element = array.find((element) => {
+    return element["result"] === "W";
+  });
+  return element ? element["year"] : undefined;
+}
